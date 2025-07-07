df8ce767a6c80955956baaa3471dd41863622c1b
GEE-MM94 <mfundomkhasibe@gmail.com>
2025-07-07 18:56:42 +0200

    Revert "added a new line to Shipping_Cost_Calculator.py"
    This reverts commit 0a5ad2fbdd1d6354c876f7e2ae5d64c0c8726fd9.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -10,5 +10,4 @@ shipping_cost = weight * rate
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
 
-# Here is a new update by GEE-MM94
-# Here is another update by GEE-MM94
+ # Here is a new update by GEE-MM94
